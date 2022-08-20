06152e7a3f10ee329565e83959b5451fe2aedbb8
Aldrian Nurrahman Raihan <raihan.mcbrain@gmail.com>
2022-08-20 13:23:23 +0700

    first commit

---
+++ login.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!--bootstrap css-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+    <!--custom css-->
+    <link href="css/style.css" rel="stylesheet">
+
+    <title>Login</title>
+  </head>
+
+  <body>
+
+    <div class="global-container">
+      <div class="card login-form">
+        <div class="card-body">
+          <h1 class="card-title text-center">Welcome</h1>
+        </div>
+        <div class="card-text">
+
+          <form>
+            <div class="mb-2">
+              <label for="exampleInputEmail1" class="form-label">Username</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-2">
+              <label for="exampleInputPassword1" class="form-label">Password</label>
+              <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="d-grid gap-2">
+            <button type="submit" class="btn btn-primary">Login</button>
+            </div>
+          </form>
+
+        </div>
+      </div>
+    </div>
+
+  </body>
+</html>
